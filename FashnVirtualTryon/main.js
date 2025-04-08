605be63de91ed978be096da3a7815b1a94dc5029
AnyPerson1 <arda45simsek@gmail.com>
2025-04-08 12:56:49 +0300

    yandan menu

--- FashnVirtualTryon/main.js
+++ FashnVirtualTryon/main.js
@@ -1,4 +1,8 @@
-function settingsPopup() {
-  const popup = document.getElementById("settingsPopup");
-  popup.classList.add("active");
+function settingsPopup() { 
+  const popup = document.getElementById("settings-container");
+  if (popup) {
+    popup.classList.toggle("active");
+  } else {
+    console.error("Element with ID 'settings-container' not found.");
+  }
 }
